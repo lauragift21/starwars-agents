1ad984ff96dc0525ce89a8fa003c2201491e2ce6
lauragift21 <laurandidi21@gmail.com>
2018-04-24 11:37:04 +0100

    chore(build): Add es6 syntax to index.js and index.test.js using babel plugin

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-var uniqueRandomArray = require('unique-random-array');
-var starWarsNames = require('./starwars-names.json');
+import uniqueRandomArray from "unique-random-array";
+import starWarsNames from "./starwars-names.json";
+
 var getRandomItem = uniqueRandomArray(starWarsNames);
 
-module.exports   = {
-    all: starWarsNames,
-    random: random
+module.exports = {
+  all: starWarsNames,
+  random: random
 };
 
 function random(number) {
-    "use strict"
-    if (number === undefined){
-        return getRandomItem();
+  "use strict";
+  if (number === undefined) {
+    return getRandomItem();
+  } else {
+    var randomItems = [];
+    for (let i = 0; i < number; i++) {
+      randomItems.push(getRandomItem());
     }
-    else {
-        var randomItems = [];
-        for (let i = 0; i < number; i++) {
-            randomItems.push(getRandomItem());
-        }
-        return randomItems;
-    }
-
+    return randomItems;
+  }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,33 +1,33 @@
-var expect = require('chai').expect;
-var starwars = require('./index');
+var expect = require("chai").expect;
+import starwars from "./index";
 
-describe('starwars-agents', function() {
-    describe('all', function(){
-        it('should be an array of strings', function(){
-            expect(starwars.all).to.satisfy(isArrayOfStrings);
+describe("starwars-agents", function() {
+  describe("all", function() {
+    it("should be an array of strings", function() {
+      expect(starwars.all).to.satisfy(isArrayOfStrings);
 
-            function isArrayOfStrings(array) {
-                return array.every(function(item) {
-                    return typeof item === 'string';
-                });
-            }
+      function isArrayOfStrings(array) {
+        return array.every(function(item) {
+          return typeof item === "string";
         });
-        it('should contain `Egwuenu Gift`', function() {
-            expect(starwars.all).to.include('Egwuenu Gift');
-        });
+      }
     });
-
-    describe('random', function() {
-        it('should return a random starwars item from starwars.all', function() {
-          var randomItem = starwars.random();
-          expect(starwars.all).to.include(randomItem);
-        });
-        it('should return an array of random items if passed', function() {
-            var randomItems = starwars.random(3);
-            expect(randomItems).to.have.length(3);
-            randomItems.forEach(function(item) {
-              expect(starwars.all).to.include(item);
-            });
-        })
+    it("should contain `Egwuenu Gift`", function() {
+      expect(starwars.all).to.include("Egwuenu Gift");
     });
+  });
+
+  describe("random", function() {
+    it("should return a random starwars item from starwars.all", function() {
+      var randomItem = starwars.random();
+      expect(starwars.all).to.include(randomItem);
+    });
+    it("should return an array of random items if passed", function() {
+      var randomItems = starwars.random(3);
+      expect(randomItems).to.have.length(3);
+      randomItems.forEach(function(item) {
+        expect(starwars.all).to.include(item);
+      });
+    });
+  });
 });
